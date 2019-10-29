653a0dce9e21e7f5ab4fa8108a4308db2c14f17f
JamesSwatton <jamessswatton@gmail.com>
2019-10-29 12:25:14 +0000

    add task to list / clear list functionality

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-loadEvents();
+document.addEventListener('DOMContentLoaded', () => {
+    loadEvents();
+});
+
 
 function loadEvents() {
-    console.log(document.querySelector('form'));
+    // console.log(document.querySelector('form'));
     document.querySelector('form').addEventListener('submit', submit);
+    document.querySelector('#clear').addEventListener('click', clearList);
 }
 
 function submit(event) {
     event.preventDefault();
-    let input = event.target.task.value;
-    console.log(input);
+    let task = event.target.task.value;
+    console.log(task);
+    addTask(task);
+}
+
+function addTask(task) {
+    let ul = document.querySelector('ul');
+    let taskListItem = document.createElement('li');
+    taskListItem.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;;
+    ul.appendChild(taskListItem);
+}
+
+function clearList() {
+    document.querySelector('ul').innerHTML = '';
 }
